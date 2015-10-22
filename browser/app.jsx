26f4f5c9b052bfea69cb743e5570c37d86a0e76b
Tim Macfarlane <timmacfarlane@gmail.com>
2015-10-22 17:48:22 +0200

    attempt7

--- browser/app.jsx
+++ browser/app.jsx
@@ -1,3 +1,48 @@
 var plastiq = require('plastiq');
 var h = plastiq.html;
 var httpism = require('httpism');
+
+module.exports = function() {
+  var app = {
+    images: [],
+
+    loadImages: function () {
+      var self = this;
+
+      return httpism.get('/images').then(function (response) {
+        self.images = response.body;
+      });
+    },
+
+    render: function () {
+      var self = this;
+
+      this.refresh = h.refresh;
+
+      return <div>
+        <ol>
+          {this.images.map(function (image) {
+            function voteUp() {
+              return httpism.post(image.voteHrefTemplate.replace('{vote}', '1')).then(function () {
+                return self.loadImages();
+              });
+            }
+
+            return <li>
+              <img src={image.url} alt="" />
+              <div class="buttons">
+                <button class="up" onclick={voteUp}></button>
+              </div>
+            </li>;
+          })}
+        </ol>
+      </div>;
+    }
+  };
+
+  app.loadImages().then(function () {
+    app.refresh();
+  });
+
+  return app;
+};
--- test/browser/imagesSpec.js
+++ test/browser/imagesSpec.js
@@ -7,3 +7,51 @@ var expect = require('chai').expect;
 var createApi = require('./api');
 require('unset-timeout');
 var retry = require('trytryagain');
+
+var images = browser.find('#test').component({
+  shouldHaveImages: function (urls) {
+    return this.find('ol li img').shouldHaveElements(function (imgs) {
+      var srcs = imgs.map(function (img) {
+        return img.getAttribute('src');
+      });
+
+      expect(srcs).to.eql(urls);
+    });
+  },
+
+  image: function (url) {
+    return this.find('ol li').containing('img[src*=' + url + ']').component(imageComponent);
+  }
+});
+
+var imageComponent = {
+  voteUpButton: function () {
+    return this.find('button.up');
+  }
+};
+
+describe('images app', function () {
+  beforeEach(function () {
+    var api = createApi();
+    api.addImage('/images/image1.gif');
+    api.addImage('/images/image2.gif');
+
+    mount(createApp());
+  });
+
+  it('can show images', function () {
+    return images.shouldHaveImages([
+      '/images/image1.gif',
+      '/images/image2.gif'
+    ]);
+  });
+
+  it('can vote an image up', function () {
+    return images.image('image2').voteUpButton().click().then(function () {
+      return images.shouldHaveImages([
+        '/images/image2.gif',
+        '/images/image1.gif'
+      ]);
+    })
+  });
+});

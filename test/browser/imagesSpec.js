c463a83ee05ed665c80438a02cc6aca183430657
Tim Macfarlane <timmacfarlane@gmail.com>
2016-11-04 10:28:15 +0800

    fixes

--- test/browser/imagesSpec.js
+++ test/browser/imagesSpec.js
@@ -1,9 +1,11 @@
 var mount = require('./mountHyperdom');
-var createApp = require('../../browser/app');
+var App = require('../../browser/app');
 var browser = require('browser-monkey');
 var expect = require('chai').expect;
 var createApi = require('./api');
 require('unset-timeout');
+
+var imagesApp = browser.find('#test');
 
 describe('images app', function () {
   it('can show images');

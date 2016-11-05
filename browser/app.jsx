9aabf17e149577197d4ad80eb2e3a321cfc214d1
Tim Macfarlane <timmacfarlane@gmail.com>
2016-11-05 11:22:52 +0800

    lateral demo

--- browser/app.jsx
+++ browser/app.jsx
@@ -3,7 +3,38 @@ var hyperdom = require('hyperdom');
 var httpism = require('httpism');
 
 module.exports = class {
+  constructor() {
+    this.images = [];
+  }
+
+  loadImages() {
+    return httpism.get('/images').then(response => {
+      this.images = response.body;
+    });
+  }
+
+  onload() {
+    return this.loadImages();
+  }
+
+  voteUp(image) {
+    return httpism.post(image.voteHrefTemplate.replace('{vote}', '1')).then(() => {
+      return this.loadImages();
+    });
+  }
+
   render() {
-    return <h1 class="splash">TDD for Front-End Hackers</h1>
+    return <div>
+      <ol>
+        {
+          this.images.map(image => {
+            return <li>
+              <img src={image.url} alt="" />
+              <div class="buttons"><button class="up" onclick={() => this.voteUp(image)}></button></div>
+            </li>
+          })
+        }
+      </ol>
+    </div>
   }
 };
--- test/browser/imagesSpec.js
+++ test/browser/imagesSpec.js
@@ -5,11 +5,47 @@ var expect = require('chai').expect;
 var createApi = require('./api');
 require('unset-timeout');
 
-var imagesApp = browser.find('#test');
+var imagesApp = browser.find('#test').component({
+  shouldHaveImages(images) {
+    return this.find('img').shouldHaveElements(elements => {
+      var srcs = elements.map(e => e.getAttribute('src'));
+
+      expect(srcs).to.eql(images);
+    });
+  },
+
+  findImage(image) {
+    return this.find('li').containing(`img[src*=${JSON.stringify(image)}]`)
+  }
+});
 
 describe('images app', function () {
-  it('can show images');
-  it('can vote an image up');
+  var api;
+
+  beforeEach(function () {
+    api = createApi();
+    api.addImage('/images/image1.gif');
+    api.addImage('/images/image2.gif');
+
+    mount(new App());
+  });
+
+  it('can show images', function () {
+    return imagesApp.shouldHaveImages([
+      '/images/image1.gif',
+      '/images/image2.gif'
+    ]);
+  });
+
+  it('can vote an image up', function () {
+    return imagesApp.findImage('image2').find('button.up').click().then(function () {
+      return imagesApp.shouldHaveImages([
+        '/images/image2.gif',
+        '/images/image1.gif',
+      ]);
+    });
+  });
+
   it('can vote an image down');
   it('can add a new image');
 });
